20db9df4e7b56dc333caa834854e80e94da24df9
Bayu Septian <ransgaseptians1970@gmail.com>
2026-02-23 02:16:49 +0700

    update backend search API

--- routes/api.php
+++ routes/api.php
@@ -5,48 +5,22 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserLoginController;
 
-/*
-|--------------------------------------------------------------------------
-| PUBLIC ROUTE
-|--------------------------------------------------------------------------
-*/
 
 Route::post('/login', [AuthController::class, 'login']);
 
 
-/*
-|--------------------------------------------------------------------------
-| PROTECTED ROUTE (WAJIB LOGIN SANCTUM)
-|--------------------------------------------------------------------------
-*/
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    /*
-    |-----------------------------------------
-    | CRUD USER LOGIN
-    |-----------------------------------------
-    */
     Route::apiResource('users', UserLoginController::class);
 
 
-    /*
-    |-----------------------------------------
-    | SEARCH DATA REALTIME
-    |-----------------------------------------
-    | C -> nama = Turner Mia
-    | D -> nim  = 9352078461
-    | E -> ymd  = 20230405
-    */
+   
    Route::get('/search/name/{nama}', [UserLoginController::class, 'searchName']);
 Route::get('/search/nim/{nim}',   [UserLoginController::class, 'searchNim']);
 Route::get('/search/ymd/{ymd}',   [UserLoginController::class, 'searchYmd']);
 
-    /*
-    |-----------------------------------------
-    | OPTIONAL: cek user login aktif
-    |-----------------------------------------
-    */
+   
     Route::get('/me', function (Request $request) {
         return response()->json($request->user());
     });
